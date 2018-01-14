cb0531643362c2c7253f44480596c9e6ef9af9eb
hfp <hfp.lan>
2018-01-14 14:24:20 +0800

    HRMS系统框架

---
+++ hrms Maven Webapp/src/main/java/com/hrms/entity/WagesStandard.java
@@ -0,0 +1,96 @@
+package com.hrms.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * WagesStandard entity. @author MyEclipse Persistence Tools
+ */
+
+public class WagesStandard implements java.io.Serializable {
+
+	// Fields
+
+	private Long id;
+	private Double basicWages;
+	private Double transportationAllowance;
+	private Double communicationAllowance;
+	private Double lunchAllowance;
+	private Set payWageses = new HashSet(0);
+
+	// Constructors
+
+	/** default constructor */
+	public WagesStandard() {
+	}
+
+	/** minimal constructor */
+	public WagesStandard(Double basicWages, Double transportationAllowance,
+			Double communicationAllowance, Double lunchAllowance) {
+		this.basicWages = basicWages;
+		this.transportationAllowance = transportationAllowance;
+		this.communicationAllowance = communicationAllowance;
+		this.lunchAllowance = lunchAllowance;
+	}
+
+	/** full constructor */
+	public WagesStandard(Double basicWages, Double transportationAllowance,
+			Double communicationAllowance, Double lunchAllowance, Set payWageses) {
+		this.basicWages = basicWages;
+		this.transportationAllowance = transportationAllowance;
+		this.communicationAllowance = communicationAllowance;
+		this.lunchAllowance = lunchAllowance;
+		this.payWageses = payWageses;
+	}
+
+	// Property accessors
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getBasicWages() {
+		return this.basicWages;
+	}
+
+	public void setBasicWages(Double basicWages) {
+		this.basicWages = basicWages;
+	}
+
+	public Double getTransportationAllowance() {
+		return this.transportationAllowance;
+	}
+
+	public void setTransportationAllowance(Double transportationAllowance) {
+		this.transportationAllowance = transportationAllowance;
+	}
+
+	public Double getCommunicationAllowance() {
+		return this.communicationAllowance;
+	}
+
+	public void setCommunicationAllowance(Double communicationAllowance) {
+		this.communicationAllowance = communicationAllowance;
+	}
+
+	public Double getLunchAllowance() {
+		return this.lunchAllowance;
+	}
+
+	public void setLunchAllowance(Double lunchAllowance) {
+		this.lunchAllowance = lunchAllowance;
+	}
+
+	public Set getPayWageses() {
+		return this.payWageses;
+	}
+
+	public void setPayWageses(Set payWageses) {
+		this.payWageses = payWageses;
+	}
+
+}
