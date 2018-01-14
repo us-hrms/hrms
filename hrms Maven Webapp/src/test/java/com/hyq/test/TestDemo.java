bcc03641e3bbc71e3f0b56d9fed48d20ddbda1f2
hfp <hfp.lan>
2018-01-14 17:30:36 +0800

    main

---
+++ hrms Maven Webapp/src/test/java/com/hyq/test/TestDemo.java
@@ -0,0 +1,20 @@
+package com.hyq.test;
+
+import org.junit.Test;
+
+/**
+ * 测试类示例
+ * @author 56474
+ * 每个服务类都写一个测试
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * 每个功能/服务层方法  都写一个方法
+	 */
+	@Test
+	void one(){
+		//编写测试主体
+	}
+}
